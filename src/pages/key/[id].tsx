07217407ea2c27f3a274d05f2161659e8d8c35f1
senabi <sensenabi@gmail.com>
2022-11-10 16:28:56 -0500

    Simple tabs

--- src/pages/key/[id].tsx
+++ src/pages/key/[id].tsx
@@ -9,42 +9,55 @@ const KeyData: React.FC<{
 }> = (props) => {
   console.log(props.data);
   return (
-    <Tabs.Root className="" defaultValue="tab1">
-      <Tabs.List className="" aria-label="Manage key">
-        <Tabs.Trigger className="" value="tab1">
-          Key
-        </Tabs.Trigger>
-        <Tabs.Trigger className="" value="tab2">
-          Public Values
-        </Tabs.Trigger>
-        {props.data.pKeyValues ? (
-          <Tabs.Trigger className="" value="tab3">
-            Private Values
+    // <div className="ml-auto mr-auto">
+    <div className="flex h-screen items-center justify-center">
+      <Tabs.Root
+        className="rounded-lg bg-[var(--colors-loContrast)] p-4"
+        defaultValue="tab1"
+      >
+        <Tabs.List
+          className="flex justify-evenly gap-2 pb-2"
+          aria-label="Manage key"
+        >
+          <Tabs.Trigger className="" value="tab1">
+            Key
           </Tabs.Trigger>
-        ) : null}
-      </Tabs.List>
-      <Tabs.Content value="tab1">
-        <div className="whitespace-pre-wrap">{props.data.raw}</div>
-      </Tabs.Content>
-      <Tabs.Content className="flex flex-col gap-2" value="tab2">
-        <div>Modulus {props.data.modulusLength}</div>
-        <div>Public Exponent {props.data.publicExponent.toString()}</div>
-        <div>Cryptographic algorithm {props.data.asymmetricKeyType}</div>
-        <div>Type {props.data.type}</div>
-      </Tabs.Content>
-      {props.data.pKeyValues !== null && (
-        <Tabs.Content className="flex flex-col gap-2" value="tab3">
-          <div>Private Exponent {props.data.pKeyValues.privateExponent}</div>
-          <div>First Prime Factor {props.data.pKeyValues.firstPrimeFactor}</div>
-          <div>
-            Second Prime Factor {props.data.pKeyValues.secondPrimeFactor}
+          <Tabs.Trigger className="" value="tab2">
+            Public Values
+          </Tabs.Trigger>
+          {props.data.pKeyValues ? (
+            <Tabs.Trigger className="" value="tab3">
+              Private Values
+            </Tabs.Trigger>
+          ) : null}
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          <div className="h-80 overflow-x-hidden whitespace-pre-wrap">
+            {props.data.raw}
           </div>
-          <div>First Exponent {props.data.pKeyValues.firstExponent}</div>
-          <div>Second Exponent {props.data.pKeyValues.secondExponent}</div>
-          <div>Coefficient {props.data.pKeyValues.coefficient}</div>
         </Tabs.Content>
-      )}
-    </Tabs.Root>
+        <Tabs.Content className="flex flex-col gap-2" value="tab2">
+          <div>Modulus {props.data.modulusLength}</div>
+          <div>Public Exponent {props.data.publicExponent.toString()}</div>
+          <div>Cryptographic algorithm {props.data.asymmetricKeyType}</div>
+          <div>Type {props.data.type}</div>
+        </Tabs.Content>
+        {props.data.pKeyValues !== null && (
+          <Tabs.Content className="flex flex-col gap-2" value="tab3">
+            <div>Private Exponent {props.data.pKeyValues.privateExponent}</div>
+            <div>
+              First Prime Factor {props.data.pKeyValues.firstPrimeFactor}
+            </div>
+            <div>
+              Second Prime Factor {props.data.pKeyValues.secondPrimeFactor}
+            </div>
+            <div>First Exponent {props.data.pKeyValues.firstExponent}</div>
+            <div>Second Exponent {props.data.pKeyValues.secondExponent}</div>
+            <div>Coefficient {props.data.pKeyValues.coefficient}</div>
+          </Tabs.Content>
+        )}
+      </Tabs.Root>
+    </div>
   );
 };
 
